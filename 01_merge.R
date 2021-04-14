c251555b484f6b2c95f5be02be8229a88d2f55ae
Piotr Pytlarz <ppytlarz@fourteen33.com>
2021-04-14 16:00:51 +0200

    rewritten, shortened

--- 01_merge.R
+++ 01_merge.R
@@ -1,75 +1,63 @@
 library(tidyverse)
-library(stringr)
-df <-  list.files(path = 'md-to-csv-files', full.names = TRUE) %>% 
+library(assertthat)
+
+all_lines <- list.files(path = 'md-to-csv-files',
+                        full.names = TRUE) %>% 
   lapply(read.csv) %>% 
   bind_rows 
 
-links <- df %>% 
+links <- all_lines %>% 
   filter(type == 'link') %>%
-  select(value,nchar,section,issue) %>%
+  select(-section_lookup, -type) %>%
   na.omit() 
 
+link_title = str_locate_all(pattern ='\\[.+\\]', links$value)
+link_url = str_locate(pattern ='\\(http.*?\\)', links$value) 
 
-link_title = str_locate_all(pattern ='\\[.+\\]',links$value)
-link_url = str_locate(pattern ='\\(http.*?\\)',links$value) 
+assert_that(length(link_title) == dim(links)[1], 
+            msg = "You have invalid links in rmarkdown files!")
 
-if(
-  length(link_title) != dim(links)[1]) 
-  stop("Error: You have invalid links in rmarkdown files!")
+assert_that(length(unique(do.call(rbind, link_title)[,1])) == 1,
+            msg = "You have link titles starting in different positions")
 
-if(
-  length(unique(do.call(rbind,link_title)[,1])) != 1)
-  stop("Error: You have link titles starting in different positions")
-
-if(
-  length(link_url) != dim(links)[1]) 
-  stop("Error: You have invalid links in rmarkdown files!")
+assert_that(dim(link_url)[1] == dim(links)[1],
+            msg = "You have invalid links in rmarkdown files!")
 
 title1 <- sapply(link_title,'[[',1)
 title2 <- sapply(link_title,'[[',2)
 
-# url1 <- sapply(link_url,'[[',1)
-# url2 <- sapply(link_url,'[[',2)
-
 url1 <- link_url[,1]
 url2 <- link_url[,2]
 
-# link_url <- do.call(rbind,link_url)
-
-link_positions <- tibble(title1,title2,url1,url2)
+link_positions <- tibble(title1, title2, url1, url2)
 links <- bind_cols(links, link_positions)
 
 links <- links %>%
-  mutate(
-    section = gsub('#', '', section),
-    title = substr(links$value, 4, links$title2 - 1),
-    url = substr(links$value, links$url1+1, links$url2 -1),
-    description = substr(links$value, links$url2+4, nchar)
-  ) %>%
-  select(issue,section,title,url,description)
-
-links <- links %>%
-  mutate(nchar = nchar(title),
-         title_c = gsub('\\(http.*','',title),
-         title = gsub('\\]','',title_c),
-         nchar1 = nchar(title_c)) %>%
-  mutate(issue = lubridate::ymd(issue)) %>%
-  select(issue,section,title,url,description)
+  mutate(section_title = gsub('#', '', section_title),
+    title = substr(value, 4, title2 - 1),
+    url = substr(value, url1+1, url2 -1),
+    description = substr(value, url2+4, nchar),
+    nchar = nchar(title),
+    title_c = gsub('\\(http.*','',title),
+    title = gsub('\\]','',title_c),
+    nchar1 = nchar(title_c),
+    issue = lubridate::ymd(issue)) %>%
+  select(issue,section_title,title,url,description)
 
 links <- links %>%
   distinct(title, url, .keep_all = TRUE)
 
-#### SECTIONS
-links$section
-links$section <- trimws(links$section)
+#### section_titleS
+links$section_title
+links$section_title <- trimws(links$section_title)
 links$description <- trimws(links$description)
 
-sections <- count(links, section)
+section_titles <- count(links, section_title)
 
-# ---- fixing different names for the same sections
+# ---- fixing different names for the same section_titles
 
 links <- links %>%
-  mutate(section = recode(section,
+  mutate(section_title = recode(section_title,
                             'R in Real World' = 'R in the Real World',
                             'R in the Real World' = 'R in the Real World',
                             'R in Organization' = 'R in Organizations',
@@ -100,109 +88,64 @@ links <- links %>%
                             'R for Fun' = '(misc)',
                             'Data' = '(misc)',
                             'Machine Learning and Statistics' = '(misc)',
-                            .missing = section
+                            .missing = section_title
   ))
 
 
-links$section <- str_replace(links$section, 
+links$section_title <- str_replace(links$section_title, 
                              pattern = '.R in Academia', 
                              replacement = 'R in Academia')
 
-links$section <- str_replace(links$section, 
+links$section_title <- str_replace(links$section_title, 
                              pattern = '.*Election.*', 
                              replacement = '(misc)')
 
+section_titles <- count(links, section_title)
 
-
-sections <- count(links, section)
-
-# str_extract(links$url, '.*\\/\\/.*[^\\/]*')
-
-
+## to extract url_base, I borrowed the idea from Josh O'Brien
 ## https://stackoverflow.com/questions/15909626/regex-grab-from-beginning-to-n-occurrence-of-character
-## Josh O'Brien answer: function
+
 matchToNth <- function(char, n) { 
   others <- paste0("[^", char, "]*") 
   mainPat <- paste0(c(rep(c(others, char), n - 1), others), collapse = "")
   paste0("(^", mainPat, ")", "(.*$)")
 }
 
-links$url_base <- str_replace(
-  str_replace(links$url, matchToNth("/", 3), '\\1'),
-  str_extract(links$url, 'https?://'),
-  ''
-  )
+glimpse(links)
 
+links <- links %>%
+  mutate(url_base = str_replace(url, matchToNth("/", 3), '\\1'),
+         url_base = str_replace(url_base,  'https?://', ''),
+         url_base = str_to_lower(url_base))
 
 url_bases <- count(links, url_base) %>%
   arrange(desc(n)) %>%
   rename(N = n)
 
-links <- left_join(links, url_bases)
-
-links$url_base <- str_replace(links$url_base, 
-                              pattern = 'shirinG.github.io', 
-                              replacement = 'shiring.github.io')
-
-section_url <- links %>%
-  count(section, url_base) %>%
+section_title_url <- links %>%
+  count(section_title, url_base) %>%
   group_by(url_base) %>%
   mutate(prop = prop.table(n)) %>%
   left_join(url_bases, by = 'url_base') %>%
   arrange(desc(N),desc(n))
 
-url_factors <- links %>%
-  arrange(N) %>%
-  distinct(url_base) %>%
-  pull(url_base)
+tutorials        <- filter(links, section_title == 'Tutorials')
+real_world       <- filter(links, section_title == 'R in the Real World')
+new_packages     <- filter(links, section_title == 'New Packages')
+package_releases <- filter(links, section_title == 'Package Releases')
+organizations    <- filter(links, section_title == 'R in Organizations')
+highlight        <- filter(links, section_title == 'Highlight')
+insights         <- filter(links, section_title == 'Insights')
+resources        <- filter(links, section_title == 'Resources')
+videos_podcasts  <- filter(links, section_title == 'Videos % Podcasts')
 
-links$url_base <- factor(links$url_base, levels = url_factors)
-
-
-links %>%
-  arrange(desc(N)) %>%
-  filter(N > 10) %>%
-  ggplot() +
-  geom_bar(aes(url_base, fill = section), position = 'fill') + 
-  coord_flip()
-
-
-
-tutorials        <- filter(links, section == 'Tutorials')
-real_world       <- filter(links, section == 'R in the Real World')
-new_packages     <- filter(links, section == 'New Packages')
-package_releases <- filter(links, section == 'Package Releases')
-organizations    <- filter(links, section == 'R in Organizations')
-highlight        <- filter(links, section == 'Highlight')
-insights         <- filter(links, section == 'Insights')
-resources        <- filter(links, section == 'Resources')
-videos_podcasts  <- filter(links, section == 'Videos % Podcasts')
-
-highquality <- filter(links, section %in% c('Highlight','Insights')) %>%
+highquality <- filter(links, section_title %in% c('Highlight','Insights')) %>%
   group_by(url_base, N) %>%
   summarise(n = n()) %>%
   mutate(ratio = n/N) %>%
   mutate(label = ifelse(n > 1 , as.character(url_base),'')) %>%
   arrange(desc(ratio)) %>%
   ungroup()
-
-
-ggplot(highquality, aes(n,N)) +
-  geom_point(aes(color = ratio, size = n), alpha = 0.6 ) +
-  geom_smooth() +
-  ggrepel::geom_label_repel(aes(label = label), size = 2 ) +
-  xlim(0,15) + ylim(0,100) 
-  
-  # geom_text(aes(label = label), size = 2 )
-
-select(highquality, n,N) %>%
-  ungroup() %>%
-ggplot(aes(N,n)) +
-  geom_point() +
-  geom_smooth()
-
-highlight <- bind_rows(highlight,highlight)
-
 
 write_csv(links,'links.csv')
 write_excel_csv(links, 'links.csv')
